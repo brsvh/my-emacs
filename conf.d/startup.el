1299855ce6581f066ab9e3208d6345c1e8c4c483
Burgess Chang <bsc@brsvh.org>
2022-06-09 04:18:16 +0800

    Configure the behaviors of files and directories
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- conf.d/startup.el
+++ conf.d/startup.el
@@ -6,4 +6,9 @@
 ;; Inhibit content in *scratch* buffer.
 (setq initial-scratch-message nil)
 
+;; Redirect storage for Emacs session.
+(setq auto-save-list-file-prefix
+      (concat (expand-file-name "sessions/" user-data-directory)
+              "session-"))
+
 ;;; startup.el ends here
