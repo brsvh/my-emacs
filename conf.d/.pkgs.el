7e9baded257b7a9c50c7916c694074089c1f7f3f
Burgess Chang <bsc@brsvh.org>
2022-06-09 19:55:00 +0800

    Redirect transient cache
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- conf.d/.pkgs.el
+++ conf.d/.pkgs.el
@@ -6,8 +6,10 @@
 (add-to-list 'package-selected-packages 'moody)
 (add-to-list 'package-selected-packages 'restart-emacs)
 (add-to-list 'package-selected-packages 'svg-lib)
+(add-to-list 'package-selected-packages 'transient)
 (add-to-list 'package-selected-packages 'which-key)
 ;; Here we provide pinned packages for `package-pinned-packages' use.
 (add-to-list 'package-pinned-packages '(consult . "gnu"))
 (add-to-list 'package-pinned-packages '(svg-lib . "gnu"))
+(add-to-list 'package-pinned-packages '(transient . "gnu"))
 (add-to-list 'package-pinned-packages '(which-key . "gnu"))
