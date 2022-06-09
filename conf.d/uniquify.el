3580851a4c0fd45f156970619fcddecb4ad5e17e
Burgess Chang <bsc@brsvh.org>
2022-06-09 20:05:10 +0800

    Change buffer name style
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

---
+++ conf.d/uniquify.el
@@ -0,0 +1,6 @@
+;;; uniquify.el --- uniquify configuration -*- lexical-binding: t; -*-
+
+;; Construct unique buffer names with forward style.
+(setq uniquify-buffer-name-style 'forward)
+
+;; uniquify.el ends here
