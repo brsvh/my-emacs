bf82c85fc7057247f0aec1716d85334957c0fb57
Burgess Chang <bsc@brsvh.org>
2022-06-09 20:00:43 +0800

    Add vertico and marginalia
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- conf.d/.pkgs.el
+++ conf.d/.pkgs.el
@@ -2,14 +2,18 @@
 ;; `package-selected-packages' use.
 (add-to-list 'package-selected-packages 'consult)
 (add-to-list 'package-selected-packages 'general)
+(add-to-list 'package-selected-packages 'marginalia)
 (add-to-list 'package-selected-packages 'minions)
 (add-to-list 'package-selected-packages 'moody)
 (add-to-list 'package-selected-packages 'restart-emacs)
 (add-to-list 'package-selected-packages 'svg-lib)
 (add-to-list 'package-selected-packages 'transient)
+(add-to-list 'package-selected-packages 'vertico)
 (add-to-list 'package-selected-packages 'which-key)
 ;; Here we provide pinned packages for `package-pinned-packages' use.
 (add-to-list 'package-pinned-packages '(consult . "gnu"))
+(add-to-list 'package-pinned-packages '(marginalia . "gnu"))
 (add-to-list 'package-pinned-packages '(svg-lib . "gnu"))
 (add-to-list 'package-pinned-packages '(transient . "gnu"))
+(add-to-list 'package-pinned-packages '(vertico . "gnu"))
 (add-to-list 'package-pinned-packages '(which-key . "gnu"))
---
+++ conf.d/marginalia.el
@@ -0,0 +1,9 @@
+;;; marginalia.el --- marginalia configuration -*- lexical-binding: t; -*-
+
+;; Make marginalia align to right. 
+(setq marginalia-align 'right)
+
+ ;; Turn on Marginalia Mode.
+(marginalia-mode +1)
+
+;;; marginalia.el ends here
