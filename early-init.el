d505b25007fc78401fda7de4adc6065617e53f73
Burgess Chang <bsc@brsvh.org>
2022-10-28 21:16:45 +0800

    feat: migrate to literature configuration
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- early-init.el
+++ early-init.el
@@ -24,9 +24,73 @@
 
 ;;; Commentary:
 
-;; TODO Describe this file.
+;; This file is load before normal init file is loaded, I configure
+;; `package' and `comp' options that the init file and my actual
+;; profile depends on here.
 
 ;;; Code:
+
+(defvar my-config-directory user-emacs-directory
+  "Directory beneath which additional config files are placed.")
+
+(defvar my-cache-directory (let ((linux '(berkeley-unix
+                                          gnu
+                                          gnu/kfreebsd
+                                          gnu/linux))
+                                 (xdg (getenv "XDG_CACHE_HOME")))
+                             (if (and (memq system-type linux) xdg)
+                                 (expand-file-name "emacs/" xdg)
+                               user-emacs-directory))
+  "Directory beneath which additional volatile files are placed.")
+
+(defvar my-data-directory (let ((linux '(berkeley-unix
+                                         gnu
+                                         gnu/kfreebsd
+                                         gnu/linux))
+                                (xdg (getenv "XDG_DATA_HOME")))
+                            (if (and (memq system-type linux) xdg)
+                                (expand-file-name "emacs/" xdg)
+                              user-emacs-directory))
+  "Directory beneath which additional non-volatile files are placed.")
+
+(defvar my-state-directory (let ((linux '(berkeley-unix
+                                         gnu
+                                         gnu/kfreebsd
+                                         gnu/linux))
+                                (xdg (getenv "XDG_STATE_HOME")))
+                            (if (and (memq system-type linux) xdg)
+                                (expand-file-name "emacs/" xdg)
+                              user-emacs-directory))
+  "Directory beneath which additional state files are placed.")
+
+;; Redirect storage location for native compilation, it must be set
+;; before all features are `require' if I want to set the follow
+;; custom directory as the first priority.
+(startup-redirect-eln-cache (concat my-cache-directory "eln-cache/"))
+
+(eval-when-compile (require 'package))
+
+;; Beginning at Emacs 27.0.50, package initialization occurs before
+;; `user-init-file' is loaded, but after `early-init-file', so some of
+;; necessary options of `package' I need to set in `early-init-file'.
+(setq package-user-dir (format "%s%s/"
+                               (concat my-cache-directory "elpa/")
+                               emacs-version)
+      package-gnupghome-dir (concat package-user-dir "gnupg/")
+      ;; Enable native ompilation support of packages.
+      package-native-compile t
+      ;; Enable precompute activation actions to speed up package
+      ;; activatation.
+      package-quickstart t
+      package-quickstart-file (concat package-user-dir "loaddefs.el"))
+
+;; Add some essentia layout parameters of frame to preset values, and
+;; ensure the minor modes corresponding to follow values are disabled.
+(push (cons 'menu-bar-lines nil) default-frame-alist)
+(push (cons 'tool-bar-lines nil) default-frame-alist)
+(push (cons 'vertical-scroll-bars nil) default-frame-alist)
+(push (cons 'horizontal-scroll-bars nil) default-frame-alist)
+(setq-default menu-bar-mode nil tool-bar-mode nil scroll-bar-mode nil)
 
 (provide 'early-init)
 ;;; early-init.el ends here
--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 
 ;; Author: Burgess Chang <bsc@brsvh.org>
 ;; Keywords: internal
-;; Package-Requires: ((emacs "29.0.50"))
+;; Package-Requires: ((emacs "29.0.50") (org "9.5.5"))
 ;; Version: 0.50.0
 
 ;; This file is part of emacs.d.
@@ -24,9 +24,38 @@
 
 ;;; Commentary:
 
-;; TODO Describe this file.
+;; This file is the first loaded file after Emacs is started.  We use
+;; literate programming to make my Emacs configuration grow, so we
+;; configure the automatic extract of the literate file.
 
 ;;; Code:
+
+(require 'early-init (concat user-emacs-directory "early-init"))
+
+(defvar my-literate-profile (concat user-emacs-directory "brsvh.org") ;
+  "Burgess Chang's literate configuration profile.")
+
+(defvar my-profile (concat my-data-directory "lisp/brsvh.el")
+  "Burgess Chang's configuration profile.")
+
+(defun my-profile-publish (file target-file)
+  "Try to export code from the literate FILE to TARGET-FILE.
+
+Optional argument TARGET-FILE can be used to specify a default
+export file for all source blocks.
+
+Return a list whose CAR is the tangled file name, and CDR is t if
+FILE is tangled, otherwise nil."
+  (unless (fboundp 'org-babel-tangle-file)
+    (autoload 'org-babel-tangle-file "ob-tangle"))
+  (eval-when-compile
+    (declare-function org-babel-tangle-file "ob-tangle"))
+  (if (or (not (file-exists-p file))
+          (file-newer-than-file-p my-literate-profile my-profile))
+      (cons (car (org-babel-tangle-file file target-file)) t)
+    (cons target-file nil)))
+
+(load (car (my-profile-publish my-literate-profile my-profile)) nil t)
 
 (provide 'init)
 ;;; init.el ends here
