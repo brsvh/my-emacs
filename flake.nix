ffef4b74e506591ddbe600f78243ce2d651a2947
Burgess Chang <bsc@brsvh.org>
2024-01-11 11:49:32 +0800

    chore: add LICENSE
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- flake.nix
+++ flake.nix
@@ -1,3 +1,19 @@
+# Copyright (C) 2023 Burgess Chang
+#
+# This file is part of emacs.d.
+#
+# emacs.d is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# emacs.d is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with emacs.d.  If not, see <https://www.gnu.org/licenses/>.
 {
   description = "emacs.d - Personal GNU Emacs configuration.";
 
@@ -304,7 +320,7 @@
                   wrappers = optionalAttrs pkgs.stdenv.isLinux {
                     tmpdir =
                       pkgs.callPackage ./nix/wrapper.nix { }
-                        "emacs.d"
+                        "emacsD"
                         emacsD-wayland;
                   };
                 };
@@ -313,7 +329,7 @@
                   wrappers = optionalAttrs pkgs.stdenv.isLinux {
                     tmpdir =
                       pkgs.callPackage ./nix/wrapper.nix { }
-                        "emacs.d"
+                        "emacsD"
                         emacsD-x11;
                   };
                 };
--- nix/flakes/devshell.nix
+++ nix/flakes/devshell.nix
@@ -1,3 +1,19 @@
+# Copyright (C) 2023 Burgess Chang
+#
+# This file is part of emacs.d.
+#
+# emacs.d is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# emacs.d is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with emacs.d.  If not, see <https://www.gnu.org/licenses/>.
 { inputs
 , ...
 }:
