ef75f26291d2bcba06e939e14d024ad1d20dff0d
Burgess Chang <bsc@brsvh.org>
2024-01-11 10:43:11 +0800

    chore: enable display-fill-column-indicator-mode when programming
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,6 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((lisp-data-mode . ((indent-tabs-mode . nil))))
+((lisp-data-mode . ((indent-tabs-mode . nil)
+                    (sentence-end-double-space . t)))
+ (prog-mode . ((eval . (display-fill-column-indicator-mode 1)))))
