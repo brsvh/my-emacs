91027efba2a9f7305430bb6665eef87da009514f
Burgess Chang <bsc@brsvh.org>
2024-01-11 11:37:35 +0800

    feat: ignore formatting checks for the elpa directory
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- nix/flakes/treefmt.nix
+++ nix/flakes/treefmt.nix
@@ -9,15 +9,27 @@
     treefmt = {
       flakeFormatter = true;
       projectRootFile = "flake.nix";
+
       programs = {
         nixpkgs-fmt = {
           enable = true;
         };
+
         shellcheck = {
           enable = true;
         };
+
         shfmt = {
           enable = true;
+        };
+      };
+
+      settings = {
+        global = {
+          excludes =
+            [
+              "elpa/**"
+            ];
         };
       };
     };
