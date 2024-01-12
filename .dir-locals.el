8ae48842d1ca166458ca4bd37d2bc2e11d66d03d
Burgess Chang <bsc@brsvh.org>
2024-01-12 19:32:24 +0800

    chore: set local variables for Org Mode
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,4 +4,7 @@
 ((auto-mode-alist . (("melpa/[^/]*$" . lisp-data-mode)))
  (lisp-data-mode . ((indent-tabs-mode . nil)
                     (sentence-end-double-space . t)))
+ (org-mode . ((eval . (auto-fill-mode 1))
+              (fill-column . 72)
+              (sentence-end-double-space . t)))
  (prog-mode . ((eval . (display-fill-column-indicator-mode 1)))))
