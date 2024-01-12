fc894caff1dfa443ea30afd32c57a33f67f772d8
Burgess Chang <bsc@brsvh.org>
2024-01-12 19:31:32 +0800

    chore: bump version
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- elpa/flake.nix
+++ elpa/flake.nix
@@ -8,6 +8,42 @@
       repo = "compat";
       type = "github";
     };
+    dash = {
+      flake = false;
+      owner = "magnars";
+      repo = "dash.el";
+      type = "github";
+    };
+    ellit-org = {
+      flake = false;
+      owner = "zevlg";
+      repo = "ellit-org.el";
+      type = "github";
+    };
+    magit-section = {
+      flake = false;
+      owner = "magit";
+      repo = "magit";
+      type = "github";
+    };
+    marginalia = {
+      flake = false;
+      owner = "minad";
+      repo = "marginalia";
+      type = "github";
+    };
+    modus-themes = {
+      flake = false;
+      owner = "protesilaos";
+      repo = "modus-themes";
+      type = "github";
+    };
+    nix-mode = {
+      flake = false;
+      owner = "NixOS";
+      repo = "nix-mode";
+      type = "github";
+    };
     vertico = {
       flake = false;
       owner = "minad";
