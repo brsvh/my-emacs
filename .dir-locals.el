dc1e17cb036ecc3fdfedcbddd18af3dad11512c9
Burgess Chang <bsc@brsvh.org>
2024-01-21 21:46:47 +0800

    chore: always display fill column indicator
    signed-off-by: Burgess Chang <bsc@brsvh.org>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,12 +1,12 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((auto-mode-alist . (("melpa/[^/]*$" . lisp-data-mode)))
+((nil . ((eval . (display-fill-column-indicator-mode 1))))
+ (auto-mode-alist . (("melpa/[^/]*$" . lisp-data-mode)))
  (lisp-data-mode . ((indent-tabs-mode . nil)
                     (sentence-end-double-space . t)))
  (org-mode . ((eval . (auto-fill-mode 1))
               (eval . (org-toggle-link-display))
               (fill-column . 72)
               (indent-tabs-mode . nil)
-              (sentence-end-double-space . t)))
- (prog-mode . ((eval . (display-fill-column-indicator-mode 1)))))
+              (sentence-end-double-space . t))))
