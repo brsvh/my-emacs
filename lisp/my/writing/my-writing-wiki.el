eb61d20c6d04bc5d481664edb2506130d02d1703
Burgess Chang <bsc@brsvh.org>
2024-03-27 14:19:52 +0800

    Add mediawiki
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

---
+++ lisp/my/writing/my-writing-wiki.el
@@ -0,0 +1,36 @@
+;;; my-writing-wiki.el --- Writing with  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2022-2024 Burgess Chang
+
+;; Author: Burgess Chang <bsc@brsvh.org>
+;; Keywords: local
+;; Package-Requires: ((emacs "29.1"))
+;; URL: https://github.com/brsvh/my-emacs
+;; Version: 0.1.50
+
+;; This file is part of my-emacs.
+
+;; my-emacs is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+
+;; my-emacs is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with my-emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; TODO describe this file.
+
+;;; Code:
+
+(use-package mediawiki
+  :vc (:url "https://github.com/hexmode/mediawiki-el.git"))
+
+(provide 'my-writing-wiki)
+;;; my-writing-wiki.el ends here
