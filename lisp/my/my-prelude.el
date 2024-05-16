7254e28f317f121d70d90d88e8e3694e9a9eac8f
Burgess Chang <bsc@brsvh.org>
2024-05-16 10:02:22 +0800

    Change the default frame size
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- lisp/my/my-prelude.el
+++ lisp/my/my-prelude.el
@@ -61,12 +61,14 @@
      my-native-lisp-directory))
   "Directory to look for natively-compiled *.eln files.")
 
-(defvar my-prelude-frame-alist '((horizontal-scroll-bars . nil)
+(defvar my-prelude-frame-alist '((height . 40)
+                                 (horizontal-scroll-bars . nil)
                                  (internal-border-width . 6)
                                  (menu-bar-lines . nil)
                                  (right-divider-width . 6)
                                  (tool-bar-lines . nil)
-                                 (vertical-scroll-bars . nil))
+                                 (vertical-scroll-bars . nil)
+                                 (width . 120))
   "Frame parameters for setup frame creation.")
 
 (defvar my-prelude-inhibit-update-load-path nil
