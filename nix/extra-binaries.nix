8a3f24b3cd2da2207945c6a907b932ae6f757f93
Burgess Chang <bsc@brsvh.org>
2024-05-21 19:40:48 +0800

    feat: add conform, editorconfig-checker, lefthook, treefmt
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- nix/extra-binaries.nix
+++ nix/extra-binaries.nix
@@ -17,13 +17,17 @@
 pkgs:
 with pkgs;
 [
+  conform
+  editorconfig-checker
   git-cliff
   graphviz
+  lefthook
   mailutils
   multimarkdown
   nixfmt-rfc-style
   ripgrep
   texliveFull
+  treefmt
 ]
 ++ (with llvmPackages; [
   bintools
